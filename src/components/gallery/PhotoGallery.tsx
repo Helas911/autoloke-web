35fef3cb5399435845ee1c354d16ffb5aea9cee9
Andrius Holovinskas <andriusholovinskas@gmail.com>
2026-02-27 17:32:10 +0200

    Fix PhotoGallery import for Vercel

--- src/components/gallery/PhotoGallery.tsx
+++ src/components/gallery/PhotoGallery.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-// Compatibility re-export for older import path used in some pages:
-// import PhotoGallery from "@/components/gallery/PhotoGallery";
-// or { PhotoGallery } ...
-
-export { PhotoGallery as default, PhotoGallery } from "../PhotoGallery";
+// Compatibility re-export for older import paths used in some pages.
+// Vercel/Next build fails if this path doesn't exist.
+export { default } from "../PhotoGallery";
+export * from "../PhotoGallery";
