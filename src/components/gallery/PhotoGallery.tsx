ae7d41ed079dfada743c3f36f398ed6ae0bf23f3
Andrius Holovinskas <andriusholovinskas@gmail.com>
2026-02-27 15:09:57 +0200

    Fix gallery import compatibility

---
+++ src/components/gallery/PhotoGallery.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+// Compatibility re-export for older import path used in some pages:
+// import PhotoGallery from "@/components/gallery/PhotoGallery";
+// or { PhotoGallery } ...
+
+export { PhotoGallery as default, PhotoGallery } from "../PhotoGallery";
